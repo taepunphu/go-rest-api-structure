9fd13b7ca66e6ca509872a90e35e7003e9f25660
Kiattisak Phanphu <kiattisak.ph@sansiri.com>
2023-07-14 17:30:56 +0700

    fixed bug name

--- utils/http.go
+++ utils/http.go
@@ -3,7 +3,7 @@ package utils
 // Get config path for local or docker
 func GetConfigPath(configPath string) string {
 	if configPath == "docker" {
-		return "./config/config-docker"
+		return "./configs/config-docker"
 	}
-	return "./config/config-local"
+	return "./configs/config-local"
 }
