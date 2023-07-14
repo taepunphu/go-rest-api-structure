9d9e3cb6432507a57103032159e0aeb67e40cfd5
Kiattisak Phanphu <kiattisak.ph@sansiri.com>
2023-07-14 15:33:59 +0700

    add new connect database :wrench:

---
+++ database/database.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/taepunphu/go-rest-api-structure/configs"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	maxOpenConns    = 60
+	connMaxLifetime = 120
+	maxIdleConns    = 30
+	connMaxIdleTime = 20
+)
+
+func NewPsqlDB(c configs.Config) (*gorm.DB, error) {
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		c.Postgres.PostgresqlHost,
+		c.Postgres.PostgresqlPort,
+		c.Postgres.PostgresqlUser,
+		c.Postgres.PostgresqlDbname,
+		c.Postgres.PostgresqlPassword,
+	)
+
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(maxOpenConns)
+	sqlDB.SetConnMaxLifetime(connMaxLifetime * time.Second)
+	sqlDB.SetMaxIdleConns(maxIdleConns)
+	sqlDB.SetConnMaxIdleTime(connMaxIdleTime * time.Second)
+	
+	if err = sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
